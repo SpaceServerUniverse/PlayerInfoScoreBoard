070a68c2b8eb917291dd1595884eed9edb79ae2f
yurisi <Dev.yurisi@gmail.com>
2023-09-24 22:55:34 +0900

    first commit

---
+++ src/main/java/space/yurisi/playerinfoscoreboard/PlayerInfoScoreBoard.java
@@ -0,0 +1,17 @@
+package space.yurisi.playerinfoscoreboard;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PlayerInfoScoreBoard extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
